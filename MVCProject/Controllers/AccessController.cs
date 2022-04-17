0c3e436a4fa7f83b7719f04baa2566a09cbb29f5
carlos richter <rrch.cr8@gmail.com>
2022-04-17 01:32:00 -0400

    Add project files.

---
+++ MVCProject/Controllers/AccessController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProject.Controllers
+{
+    public class AccessController : Controller
+    {
+        // GET: Access
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Enter(string user, string password)
+        {
+            try
+            {
+                return Content("1");
+            }
+            catch (Exception ex)
+            { 
+                return Content(ex.Message);
+            }
+   
+
+        }
+    }
+}
