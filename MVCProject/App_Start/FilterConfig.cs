98338aa732530ec80b42b570a7d35c1642d3e851
carlos richter <rrch.cr8@gmail.com>
2022-04-20 10:27:52 -0400

    Curso de MVC .Net C#| Filtros, seguridad | #3

--- MVCProject/App_Start/FilterConfig.cs
+++ MVCProject/App_Start/FilterConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using MVCProject.Filters;
+using System.Web;
 using System.Web.Mvc;
 
 namespace MVCProject
@@ -8,6 +9,7 @@ namespace MVCProject
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new VerifySession());
         }
     }
 }
---
+++ MVCProject/Filters/VerifySession.cs
@@ -0,0 +1,33 @@
+﻿using MVCProject.Controllers;
+using MVCProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProject.Filters
+{
+    public class VerifySession : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var oUser = (Users)HttpContext.Current.Session["User"];
+            if (oUser == null)
+            {
+                if (filterContext.Controller is AccessController == false)
+                {
+                    filterContext.HttpContext.Response.Redirect("~/Access/Index");
+                }
+            }
+            else
+            {
+                if (filterContext.Controller is AccessController == true)
+                {
+                    filterContext.HttpContext.Response.Redirect("~/Home/Index");
+                }
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
